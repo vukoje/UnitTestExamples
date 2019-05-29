d32dc466b71f9ec80ef997091de3612e663efb11
Milan Vukoje <milan.vukoje@sap.com>
2019-05-30 00:18:54 +0200

    Refactoring to use NSubstitute.

--- UnitTestExamples.Tests/Calculation/CalculatorTest.cs
+++ UnitTestExamples.Tests/Calculation/CalculatorTest.cs
@@ -1,17 +1,18 @@
 ﻿using NUnit.Framework;
 using UnitTestExamples.Calculation;
+using NSubstitute;
 
 namespace UnitTestExamples.Tests.Calculation
 {
     public class CalculatorGetPriceTest
     {
         private Calculator Calculator { get; set; }
-        public CalculatorHelperStub Stub { get; set; }
+        public ICalculatorHelper Stub { get; set; }
 
         [SetUp]
         public void SetUp()
         {
-            this.Stub = new CalculatorHelperStub();
+            this.Stub = Substitute.For<ICalculatorHelper>();
             this.Calculator = new Calculator(this.Stub);
         }
 
@@ -19,7 +20,7 @@ namespace UnitTestExamples.Tests.Calculation
         public void ActionPriceTest()
         {
             decimal actionPrice = 100;
-            this.Stub.GetPriceOnActionHandler = x => { return actionPrice; };
+            this.Stub.GetPriceOnAction(Arg.Any<int>()).Returns(actionPrice);
 
             this.TestGetPrice(actionPrice, "If action price exists it Should be used as price.");
         }
@@ -28,8 +29,8 @@ namespace UnitTestExamples.Tests.Calculation
         public void VolumeDiscountPriceTest()
         {
             decimal volumePrice = 1;
-            this.Stub.GetPriceOnActionHandler = x => { return null; };
-            this.Stub.GetPriceWithVolumeDiscountHandler = (x, y) => { return volumePrice; };
+            this.Stub.GetPriceOnAction(Arg.Any<int>()).Returns((decimal?)null);
+            this.Stub.GetPriceWithVolumeDiscount(Arg.Any<int>(), Arg.Any<int>()).Returns(volumePrice);
 
             this.TestGetPrice(volumePrice, "If volume price exists and action price doesn't, volume price Should be used as price.");
         }
@@ -38,9 +39,9 @@ namespace UnitTestExamples.Tests.Calculation
         public void StandardPriceTest()
         {
             decimal standardPrice = 1;
-            this.Stub.GetPriceOnActionHandler = x => { return null; };
-            this.Stub.GetPriceWithVolumeDiscountHandler = (x, y) => { return null; };
-            this.Stub.GetStandardPriceHandler = x => { return standardPrice; };
+            this.Stub.GetPriceOnAction(Arg.Any<int>()).Returns((decimal?)null);
+            this.Stub.GetPriceWithVolumeDiscount(Arg.Any<int>(), Arg.Any<int>()).Returns((decimal?)null);
+            this.Stub.GetStandardPrice(Arg.Any<int>()).Returns(standardPrice);
 
             this.TestGetPrice(standardPrice, "If volume price and action price don't exist, standard price Should be used as price.");
         }
